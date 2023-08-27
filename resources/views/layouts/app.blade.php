6efbd13efd85fe482c2a40ae7831dcbbc6f6c996
Edy Susilo <masedysu69@gmail.com>
2023-08-27 23:48:41 +0700

    fix delete

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
     <title>{{ config('app.name') }} | @yield('title')</title>
 
     <link rel="stylesheet" href="assets/css/main/app.css">
