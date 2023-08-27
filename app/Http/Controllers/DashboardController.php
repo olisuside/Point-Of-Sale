9b4b41975370953a7936c7194964f0c446b03957
Edy Susilo <masedysu69@gmail.com>
2023-08-28 00:56:09 +0700

    update dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kategori;
+use App\Models\Produk;
+use App\Models\Supplier;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('dashboard.dashboard')->with([
+        $kategori = Kategori::count();
+        $produk = Produk::count();
+        $supplier = Supplier::count();
+
+        return view('dashboard.dashboard', compact('kategori', 'produk', 'supplier',))->with([
             'user' => Auth::user(),
         ]);
     }
--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -5,6 +5,25 @@
 @section('contents')
 
     <div class="row">
+
+        <div class="col-6 col-lg-3 col-md-6">
+            <div class="card">
+                <div class="card-body px-4 py-4-5">
+                    <div class="row">
+                        <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
+                            <div class="stats-icon green mb-2">
+                                <i class="iconly-boldCategory"></i>
+                            </div>
+                        </div>
+                        <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
+                            <h6 class="text-muted font-semibold">Kategori</h6>
+                            <h6 class="font-extrabold mb-0">{{ $kategori }}</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
         <div class="col-6 col-lg-3 col-md-6">
             <div class="card">
                 <div class="card-body px-4 py-4-5">
@@ -16,12 +35,13 @@
                         </div>
                         <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                             <h6 class="text-muted font-semibold">Produk</h6>
-                            <h6 class="font-extrabold mb-0">112</h6>
+                            <h6 class="font-extrabold mb-0">{{ $produk }}</h6>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        
         <div class="col-6 col-lg-3 col-md-6">
             <div class="card">
                 <div class="card-body px-4 py-4-5">
@@ -56,23 +76,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-6 col-lg-3 col-md-6">
-            <div class="card">
-                <div class="card-body px-4 py-4-5">
-                    <div class="row">
-                        <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
-                            <div class="stats-icon green mb-2">
-                                <i class="iconly-boldUser"></i>
-                            </div>
-                        </div>
-                        <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                            <h6 class="text-muted font-semibold">Member</h6>
-                            <h6 class="font-extrabold mb-0">112</h6>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        
     </div>
 
     {{-- Chart --}}
