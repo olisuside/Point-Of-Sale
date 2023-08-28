e524dc33275b06c2b719535c2640114d6f35c550
Edy Susilo <masedysu69@gmail.com>
2023-08-29 00:46:34 +0700

    update

--- resources/views/pembelian/index.blade.php
+++ resources/views/pembelian/index.blade.php
@@ -3,116 +3,151 @@
 @section('title', 'Pembelian')
 
 @section('contents')
-<section class="row">
-    <div class="col-12 col-lg-12">
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header d-flex justify-content-between">
-                        <h4>Data Pembelian</h4>
-                        <a class="btn btn-sm btn-primary" title="Tambah Pembelian" onclick="addForm()"><i
-                            class="bi bi-plus"></i></a>
-                    </div>
+    <section class="row">
+        <div class="col-12 col-lg-12">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header d-flex justify-content-between">
+                            <h4>Data Pembelian</h4>
+                            <div class="btn-group">
 
-                    <div class="card-body">
-                        @if ($message = Session::get('success'))
-                            <div class="alert alert-success alert-dismissible show fade">
-                                {{ $message }}
-                                <button type="button" class="btn-close" data-bs-dismiss="alert"
-                                    aria-label="Close"></button>
-                            </div>
-                        @endif
-                        <table class="table table-striped" id="table-pembelian">
-                            <thead>
-                                <tr>                       
-                                    <th>Tanggal</th>
-                                    <th>Supplier</th>
-                                    <th>Total Item</th>
-                                    <th>Total Harga</th>
-                                    <th>Diskon</th>
-                                    <th>Total Bayar</th>
-                                    <th>Aksi</th>
-                                </tr>
-                            </thead>
-                        </table>
+                                <a class="btn btn-sm btn-primary" title="Tambah Pembelian" onclick="addForm()"><i
+                                    class="bi bi-plus"></i></a>
+                                    @empty(!session('id_pembelian'))
+                                    <a href="{{ route('pembelian_detail.index') }}" class="btn btn-info btn-sm btn-flat"><i
+                                        class="bi bi-pencil"></i> Transaksi Terakhir</a>
+                                    @endempty
+                                    </div>
+                        </div>
+
+                        <div class="card-body">
+                            @if ($message = Session::get('success'))
+                                <div class="alert alert-success alert-dismissible show fade">
+                                    {{ $message }}
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                        aria-label="Close"></button>
+                                </div>
+                            @endif
+                            <table class="table table-striped" id="table-pembelian">
+                                <thead>
+                                    <tr>
+                                        <th>Tanggal</th>
+                                        <th>Supplier</th>
+                                        <th>Total Item</th>
+                                        <th>Total Harga</th>
+                                        <th>Diskon</th>
+                                        <th>Total Bayar</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    
-    
-    
-    @include('pembelian.supplier')
-    @include('pembelian.detail')
-</section>
+
+
+
+        @include('pembelian.supplier')
+        @include('pembelian.detail')
+    </section>
 @endsection
 
 @push('scripts')
-<script>
-    let table, table1;
+    <script>
+        let table, table1;
 
-    $(function () {
-        table = $('#table-pembelian').DataTable({
-            responsive: true,
-            processing: true,
-            serverSide: true,
-            autoWidth: false,
-            ajax: {
-                url: '{{ route('pembelian.data') }}',
-            },
-            columns: [
-                {data: 'tanggal'},
-                {data: 'supplier'},
-                {data: 'total_item'},
-                {data: 'total_harga'},
-                {data: 'diskon'},
-                {data: 'bayar'},
-                {data: 'aksi', searchable: false, sortable: false},
-            ]
+        $(function() {
+            table = $('#table-pembelian').DataTable({
+                responsive: true,
+                processing: true,
+                serverSide: true,
+                autoWidth: false,
+                ajax: {
+                    url: '{{ route('pembelian.data') }}',
+                },
+                columns: [{
+                        data: 'tanggal'
+                    },
+                    {
+                        data: 'supplier'
+                    },
+                    {
+                        data: 'total_item'
+                    },
+                    {
+                        data: 'total_harga'
+                    },
+                    {
+                        data: 'diskon'
+                    },
+                    {
+                        data: 'bayar'
+                    },
+                    {
+                        data: 'aksi',
+                        searchable: false,
+                        sortable: false
+                    },
+                ]
+            });
+
+            $('#table-supplier').DataTable();
+            table1 = $('#table-detail').DataTable({
+                processing: true,
+                bSort: false,
+                dom: 'Brt',
+                columns: [{
+                        data: 'DT_RowIndex',
+                        searchable: false,
+                        sortable: false
+                    },
+                    {
+                        data: 'kode_produk'
+                    },
+                    {
+                        data: 'nama_produk'
+                    },
+                    {
+                        data: 'harga_beli'
+                    },
+                    {
+                        data: 'jumlah'
+                    },
+                    {
+                        data: 'subtotal'
+                    },
+                ]
+            })
         });
 
-        $('#table-supplier').DataTable();
-        table1 = $('#table-detail').DataTable({
-            processing: true,
-            bSort: false,
-            dom: 'Brt',
-            columns: [
-                {data: 'DT_RowIndex', searchable: false, sortable: false},
-                {data: 'kode_produk'},
-                {data: 'nama_produk'},
-                {data: 'harga_beli'},
-                {data: 'jumlah'},
-                {data: 'subtotal'},
-            ]
-        })
-    });
-
-    function addForm() {
-        $('#modal-supplier').modal('show');
-    }
-
-    function showDetail(url) {
-        $('#modal-detail').modal('show');
-
-        table1.ajax.url(url);
-        table1.ajax.reload();
-    }
-
-    function deleteData(url) {
-        if (confirm('Yakin ingin menghapus data terpilih?')) {
-            $.post(url, {
-                    '_token': $('[name=csrf-token]').attr('content'),
-                    '_method': 'delete'
-                })
-                .done((response) => {
-                    table.ajax.reload();
-                })
-                .fail((errors) => {
-                    alert('Tidak dapat menghapus data');
-                    return;
-                });
+        function addForm() {
+            $('#modal-supplier').modal('show');
         }
-    }
-</script>
+
+        function showDetail(url) {
+            $('#modal-detail').modal('show');
+
+            table1.ajax.url(url);
+            table1.ajax.reload();
+        }
+
+        function deleteData(url) {
+            if (confirm('Yakin ingin menghapus data terpilih?')) {
+                $.post(url, {
+                        '_token': $('[name=csrf-token]').attr('content'),
+                        '_method': 'delete'
+                    })
+                    .done((response) => {
+                        table.ajax.reload();
+                    })
+                    .fail((errors) => {
+                        alert('Tidak dapat menghapus data');
+                        return;
+                    });
+            }
+        }
+    </script>
 @endpush
