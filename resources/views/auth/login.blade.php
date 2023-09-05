2bcab0d5cb3ca52a09d019945b0ea2a211c23aae
Edy Susilo <masedysu69@gmail.com>
2023-09-05 23:24:47 +0700

    update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,6 +3,19 @@
 @section('title', 'Login')
 
 @section('contents')
+
+
+    @if ($errors->any())
+        <div class="alert alert-danger">Whoops! Something went wrong
+
+            <ul class="">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     @if (session('status'))
         <div class="alert alert-danger">
             {{ session('status') }}
@@ -16,10 +29,11 @@
         <div class="form-body">
             <div class="row">
                 <div class="col-md-3">
-                    <label class="col-form-label">Username</label>
+                    <label class="col-form-label">Email</label>
                 </div>
                 <div class="col-md-9 form-group">
-                    <input id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus autocomplete="username">
+                    <input id="email" class="form-control" type="email" name="email" :value="old('email')" required
+                        autofocus autocomplete="username">
                 </div>
 
 
@@ -28,7 +42,8 @@
                 </div>
                 <div class="col-md-9 form-group">
                     <div class="input-group">
-                        <input  class="form-control" id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                        <input class="form-control" id="password" class="block mt-1 w-full" type="password" name="password"
+                            required autocomplete="current-password" />
                         <a class="btn icon btn-secondary" onclick="myFunction()">
                             <i class="bi bi-eye"></i>
                         </a>
