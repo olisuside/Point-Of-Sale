1967acb20f7340ff647380a166612e83236fd7d6
Edy Susilo <masedysu69@gmail.com>
2023-09-22 02:20:47 +0700

    add notification

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -10,56 +10,44 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                @php
+                    $produk = \App\Models\Produk::where('stok', 0)->get();
+                    $jumlahHabis = count($produk);
+                @endphp
                 <ul class="navbar-nav ms-auto mb-lg-0">
-                    <li class="nav-item dropdown me-1">
-                        <a class="nav-link active dropdown-toggle text-gray-600" href="#"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            <i class='bi bi-envelope bi-sub fs-4'></i>
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
-                            <li>
-                                <h6 class="dropdown-header">Mail</h6>
-                            </li>
-                            <li><a class="dropdown-item" href="#">No new mail</a></li>
-                        </ul>
-                    </li>
+
                     <li class="nav-item dropdown me-3">
                         <a class="nav-link active dropdown-toggle text-gray-600" href="#"
                             data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
-                            <i class='bi bi-bell bi-sub fs-4'></i>
+                            <i class='bi bi-bell bi-sub fs-4' style="position: relative;"></i>
+                            @if ($jumlahHabis > 0)
+                          
+                                <span class="badge badge-notification bg-danger" ">{{ $jumlahHabis }}</span>
+                            @endif
+
                         </a>
-                        <ul class="dropdown-menu dropdown-menu-end notification-dropdown"
+                        <ul class="dropdown-menu dropdown-menu-end notification-dropdown" style="left: -5rem;"
                             aria-labelledby="dropdownMenuButton">
                             <li class="dropdown-header">
                                 <h6>Notifications</h6>
                             </li>
-                            <li class="dropdown-item notification-item">
-                                <a class="d-flex align-items-center" href="#">
-                                    <div class="notification-icon bg-primary">
-                                        <i class="bi bi-cart-check"></i>
-                                    </div>
-                                    <div class="notification-text ms-4">
-                                        <p class="notification-title font-bold">Successfully check out</p>
-                                        <p class="notification-subtitle font-thin text-sm">Order ID #256</p>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="dropdown-item notification-item">
-                                <a class="d-flex align-items-center" href="#">
-                                    <div class="notification-icon bg-success">
-                                        <i class="bi bi-file-earmark-check"></i>
-                                    </div>
-                                    <div class="notification-text ms-4">
-                                        <p class="notification-title font-bold">Homework submitted</p>
-                                        <p class="notification-subtitle font-thin text-sm">Algebra math
-                                            homework</p>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <p class="text-center py-2 mb-0"><a href="#">See all notification</a>
-                                </p>
-                            </li>
+
+
+                            @foreach ($produk as $produk)
+                                <li class="dropdown-item notification-item">
+                                    <a class="d-flex align-items-center" href="#">
+                                        <div class="notification-icon bg-danger">
+                                            <i class="bi bi-exclamation-circle" ></i>
+                                        </div>
+                                        <div class="notification-text ms-4">
+                                            <p class="notification-title font-bold">Stok Habis</p>
+                                            <p class="notification-subtitle font-thin text-sm">Stok barang
+                                                "{{ $produk->nama_produk }}" telah habis.</p>
+                                        </div>
+                                    </a>
+                                </li>
+                            @endforeach
+
                         </ul>
                     </li>
                 </ul>
@@ -82,15 +70,17 @@
                         <li>
                             <h6 class="dropdown-header">Hello, {{ Auth::user()->name }}</h6>
                         </li>
-                        <li><a class="dropdown-item" href="{{ url('profil') }}"><i class="icon-mid bi bi-person me-2"></i> My
+                        <li><a class="dropdown-item" href="{{ url('profil') }}"><i
+                                    class="icon-mid bi bi-person me-2"></i> My
                                 Profile</a></li>
-                        
+
                         <li>
                             <hr class="dropdown-divider">
                         </li>
-                        <a class="dropdown-item" href="#" onclick="$('#logout-form').submit()"><i class="icon-mid bi bi-box-arrow-left me-2"></i>
+                        <a class="dropdown-item" href="#" onclick="$('#logout-form').submit()"><i
+                                class="icon-mid bi bi-box-arrow-left me-2"></i>
                             Logout</a></li>
-                            <li> 
+                        <li>
                     </ul>
                 </div>
             </div>
